2cc5587aff57b3bffc2ada64fbfee7e85b87a31a
magic3007 <maijing3007@qq.com>
2020-02-27 16:38:37 +0800

    [lab1/dhrystone-2.1] add dhrystone_profile.sh

---
+++ lab1/dhrystone-2.1/dhrystone_profile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+loops=(100000000 300000000 500000000 700000000 900000000)
+programs=(./gcc_dry2 ./gcc_dry2reg)
+num_repeats=3
+LOG_FILE=log.txt
+
+
+for prog in ${programs[@]}; do
+    for loop in ${loops[*]};  do
+        echo $prog $loop ":" | tee -a $LOG_FILE
+        for ((i=1; i<=$num_repeats; i ++)) do
+            echo "      Epoch " $i ":" $(echo $loop | $prog | grep "Dhrystones per Second:" | tr -cd "[0-9.]")  | tee -a $LOG_FILE
+        done
+    done
+done
